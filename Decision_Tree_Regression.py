4875026984f725ade64e65a74303f0a6a1f7a1f8
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-09-24 14:47:48 -0600

    Import libraries and dataset

--- Decision_Tree_Regression.py
+++ Decision_Tree_Regression.py
@@ -2,9 +2,15 @@
 # Input by Ryan L Buchanan 23SEP20
 
 # Import libraries
-
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
 # Import dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
 
 
 # Train the Decision Tree Regression model
