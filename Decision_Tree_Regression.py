40bf52a3455ec25d4afdfa7b13320efe0fa1dfa2
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-09-24 14:49:33 -0600

    Inspect dataset by ptint function

--- Decision_Tree_Regression.py
+++ Decision_Tree_Regression.py
@@ -11,7 +11,9 @@ dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
+print(X)
 
+print(y)
 
 # Train the Decision Tree Regression model
 
